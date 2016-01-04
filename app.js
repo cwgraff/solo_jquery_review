26831e6da85bafebb1a79b80f4564f2c12cfe229
cwgraff <cwgraff@Christophers-MacBook-Pro.local>
2016-01-04 12:29:04 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ * Created by cwgraff on 1/4/16.
+ */
+var storeData;
+
+$.ajax('data.json').then(function(response){
+
+    storeData = response;
+
+    for(var i=0; i < response.length; i++) {
+
+        var id = storeData[i].id;
+        var gender = storeData[i].gender;
+        var firstName = storeData[i].first_name;
+        var lastName = storeData[i].last_name;
+        var eMail = storeData[i].email;
+
+        //console.log(firstName);
+
+        $('.appendHere').append("<br/><br/>ID #" + id + "<br/>Gender: " + gender + "<br/>Name: " + firstName + " " + lastName + "<br/>E-Mail: " + eMail);
+
+    }
+
+
+
+}
+
+
+
+);
